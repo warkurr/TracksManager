03a2126f484e41b933bba438b1e5d9d21e3e9b05
warkurr <86894721+warkurr@users.noreply.github.com>
2026-09-02 16:09:06 +0200

    Inject shared app model into SwiftUI environment

--- TracksManagerApp/TracksManagerApp.swift
+++ TracksManagerApp/TracksManagerApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct TracksManagerApp: App {
+    @State private var model = AppModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(model)
         }
         .windowStyle(.automatic)
     }
