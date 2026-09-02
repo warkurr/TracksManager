4e14d9408778a09db184775e68b9d79c06318dfe
warkurr <86894721+warkurr@users.noreply.github.com>
2026-09-02 04:27:16 +0200

    Add SwiftUI app entry point

---
+++ TracksManagerApp/TracksManagerApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TracksManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.automatic)
+    }
+}
